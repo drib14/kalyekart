5ef2af251729d1933af0635a3a728b2f8e177722
drib14 <jhondrib7@gmail.com>
2025-09-21 18:43:52 +0800

    changes

--- backend/controllers/payment.controller.js
+++ backend/controllers/payment.controller.js
@@ -18,7 +18,7 @@ export const createCheckoutSession = async (req, res) => {
 
 			return {
 				price_data: {
-					currency: "usd",
+					currency: "php",
 					product_data: {
 						name: product.name,
 						images: [product.image],
